33b234f81e3a0a93bea75c8c809113f6d2e82bfc
Diogo <dmiguel.am@gmail.com>
2022-09-16 22:57:33 +0100

    Adding new configuration server for spgest application

---
+++ src/test/java/com/spgest/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spgest.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
